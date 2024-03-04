f7c2c1572b1bfc9bcca06348a3a8fb035c79be42
Luis Brodely <luisbrodely@advantedigital.com>
2024-03-04 11:07:52 -0600

    Recu

--- src/logic/examples.js
+++ src/logic/examples.js
@@ -1,14 +1,14 @@
 export const ExamplesCodes = [
   {
     title: "EJEMPLO",
-    code: `automata estado: 4 - 321 ; inicio: 2; aceptacion : 1111; fin;`,
+    code: `automata estado: 4 - 321 ; inicio: 2; aceptacion : 1111; fin`,
   },
   {
     title: "EJEMPLO 2",
-    code: `automata estado: 9 - 565 ; inicio: 4; aceptacion : 2024; fin;`,
+    code: `automata estado: 9 - 565 ; inicio: 4; aceptacion : 2024; fin`,
   },
   {
     title: "EJEMPLO 3",
-    code: `automata estado: 4 - 564 ; inicio: 2; aceptacion : 232323; fin;`,
+    code: `automata estado: 4 - 564 ; inicio: 2; aceptacion : 232323; fin`,
   },
 ];
--- src/logic/productions.js
+++ src/logic/productions.js
@@ -2,15 +2,16 @@ export function obtenerProduccion(noTerminal, siguiente) {
 	console.log('siguiente', siguiente)
 
 	const producciones = {
-		S: ["A", "B", ":", "D", "-", "C", ";", "Q", ":", "D", ";", "P", ":", "C", ";", "F"],
+		S: ["A", "B", "V"],
 		A: ["a", "u", "t", "o", "m", "a", "t", "a"],
-		B: ["e", "s", "t", "a", "d", "o"],
+		B: ["e", "s", "t", "a", "d", "o", ":", "D", "-", "C", ";", "Q", ":", "D", ";", "P", ":", "DC", ";",],
 		D: /[0-9]/.test(siguiente) ? [siguiente] : null,
 		C: /[0-9]/.test(siguiente) ? ["D", "C"] : ["ε"],
+		DC: /[0-9]/.test(siguiente) ? ["D", "C"] : null,
 		Q: ["i", "n", "i", "c", "i", "o"],
 		P: ["a", "c", "e", "p", "t", "a", "c", "i", "o", "n"],
-		F: ["f","i","n",";"]
-	};
+		V: ["f", "i", "n"],
+	  };	
   
 	return producciones[noTerminal];
 }
